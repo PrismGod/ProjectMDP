fcaf2c423a757a66b8dcc8abc00101d30b1b71e2
PrismGod <64731105+PrismGod@users.noreply.github.com>
2021-12-04 09:14:36 +0700

    bottom nav home udah selesai
    bottom nav home udah selesai

--- ProjectAndroid/app/src/main/java/com/example/projectandroid/UserActivity.java
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/UserActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,36 +19,51 @@ public class UserActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user2);
 
-//        bn = findViewById(R.id.btmnav_user);
-//        bn.setOnNavigationItemSelectedListener(UserActivity.this);
-//        bn.setSelectedItemId(R.id.btmnavuser_movie);
+        bn = findViewById(R.id.btmnav_user);
+        bn.setOnNavigationItemSelectedListener(UserActivity.this);
+        bn.setSelectedItemId(R.id.btmnavuser_movie);
 
-//        Fragment f = new UserFavourite();
-//        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
+        Fragment f = new UserFavourite();
+        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        if (item.getItemId() == R.id.btmnavuser_fav){
-//            Fragment f=new UserFavourite();
-//            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
-//            return true;
-//        }
-//        if (item.getItemId() == R.id.btmnavuser_movie){
-//            Fragment f=new UserMovieFragment();
-//            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
-//            return true;
-//        }
-//        if (item.getItemId() == R.id.btmnavuser_rating){
-//            Fragment f=new UserRatingFragment();
-//            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
-//            return true;
-//        }
-//        if (item.getItemId() == R.id.btmnavuser_watchlist){
-//            Fragment f=new UserWatchListFragment();
-//            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
-//            return true;
-//        }
+        if (item.getItemId() == R.id.btmnavuser_fav){
+            Fragment f=new UserFavourite();
+            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
+            return true;
+        }
+        if (item.getItemId() == R.id.btmnavuser_movie){
+            Fragment f=new UserMovieFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
+            return true;
+        }
+        if (item.getItemId() == R.id.btmnavuser_rating){
+            Fragment f=new UserRatingFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
+            return true;
+        }
+        if (item.getItemId() == R.id.btmnavuser_watchlist){
+            Fragment f=new UserWatchListFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentUser,f).commit();
+            return true;
+        }
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menulogout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menulogout_logout)
+        {
+            finish();
+        }
+        return true;
     }
 }
