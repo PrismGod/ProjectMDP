bf4b562dfa6610e4bfa8162d9f5b92ada55a7a5e
PrismGod <64731105+PrismGod@users.noreply.github.com>
2021-12-08 11:08:04 +0700

    a
    a

--- ProjectAndroid/app/src/main/java/com/example/projectandroid/MahasiswaAdapter.java
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/MahasiswaAdapter.java
@@ -3,6 +3,7 @@ package com.example.projectandroid;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaAdapter.Maha
 
     private ArrayList<Mahasiswa> listMhs;
     TextView tname,tpass,tstatus;
+    Button b1,b2;
 
     public MahasiswaAdapter(ArrayList<Mahasiswa> listMhs) {
         this.listMhs = listMhs;
@@ -45,11 +47,28 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaAdapter.Maha
             tname =itemView.findViewById(R.id.data_username);
             tpass =itemView.findViewById(R.id.data_password);
             tstatus=itemView.findViewById(R.id.data_status);
+            b1=itemView.findViewById(R.id.item_btn_ban);
+            b2=itemView.findViewById(R.id.item_btn_unban);
+
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+
+            b2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
         }
 
         public void bind(Mahasiswa mhs) {
             tname.setText("Username : "+mhs.Username);
             tpass.setText("Password : "+mhs.Password);
+
 
             if(mhs.banned.equals("1"))
             {
